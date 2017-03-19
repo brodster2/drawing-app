a8c50551429942cc61d2fd541f318b2ef8bca7ec
Brodie <brodster22@live.com>
2017-03-19 21:03:31 +0000

    slider functionality
    Set slider to change the size of #circle in #brushDisplay.

--- logic.js
+++ logic.js
@@ -1,4 +1,13 @@
 $(function(){
 //    Create slider to set brush size
-    $('#brushslider').slider();
+    $('#brushSlider').slider({
+        max: 30,
+        min: 3,
+    });
+    
+//    Function to change #circle height & width when slider is moved
+    $('#brushSlider').on("slide", function( event, ui ) {
+       $('#circle').height(ui.value); 
+       $('#circle').width(ui.value); 
+    });
 });
